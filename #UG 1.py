7e3e4e767461bbd0438c2d29350619d38fa1e8ba
Nathan Ariel Phedra <93307812+nathanriel16@users.noreply.github.com>
2022-08-29 19:18:43 +0700

    Add files via upload

---
+++ #UG 1.py
@@ -0,0 +1,30 @@
+#UG 1
+print("Kalkulator")
+print("1. Tambah")
+print("2. Kurang")
+print("3. Kali")
+print("4. Bagi")
+
+operasi = input("Pilih operasi perhitungan : ")
+bilangan_1 = eval(input('Masukkan bilangan pertama : '))
+bilangan_2 = eval(input('Masukkan bilangan kedua : '))
+
+def kalkulator_sederhana():
+    if operasi == '1':
+        hasil = bilangan_1 + bilangan_2
+        print(hasil)
+
+    elif operasi == '2':
+        hasil = bilangan_1 - bilangan_2
+        print(hasil)
+
+    elif operasi == '3':
+        hasil = bilangan_1 * bilangan_2
+        print(hasil)
+
+    elif operasi == '4':
+        hasil = bilangan_1 / bilangan_2
+        print(hasil)
+    return
+
+kalkulator_sederhana()
